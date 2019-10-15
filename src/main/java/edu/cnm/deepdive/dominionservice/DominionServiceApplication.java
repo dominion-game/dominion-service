cb39fe2014aa926c082e1b8aea7af825bfd2cfb5
dannyseymour <daniel@discordia.io>
2019-10-15 14:52:13 -0600

    initial commit

---
+++ src/main/java/edu/cnm/deepdive/dominionservice/DominionServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.dominionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominionServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DominionServiceApplication.class, args);
+  }
+
+}
