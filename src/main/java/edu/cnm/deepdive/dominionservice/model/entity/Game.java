5fcd0b535aec33c0add82ed373bcf7081cadb991
edubois9119 <erica.dubois9119@gmail.com>
2019-10-20 17:28:21 -0600

    edited JavaDoc comments to Stack and Game

--- src/main/java/edu/cnm/deepdive/dominionservice/model/entity/Game.java
+++ src/main/java/edu/cnm/deepdive/dominionservice/model/entity/Game.java
@@ -10,6 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 
+/**
+ * The game is responsible for the following:
+ * creating a new game
+ * keeping track of players
+ * keep track of stacks
+ * determining end of game
+ */
 @Entity
 public class Game {
 
@@ -22,33 +29,60 @@ public class Game {
   private Long id;
 
   /**
-   * Returns a list of stacks.
+   * Returns a list of stacks. This list documents all of the stacks available to the players. It
+   * will be updated throughout the game as players pull cards for the stacks.
    */
   @OneToMany(mappedBy = "game", cascade = CascadeType.ALL)
   private List<Stack> stacks= new LinkedList<>();
 
   /**
-   * Returns a list of Players.
+   * Returns a list of Players. This allows for keeping track of players, turns, and points
+   * throughout the game.
    */
   @OneToMany(mappedBy= "game", cascade = CascadeType.ALL)
   private List<Player> players = new LinkedList<>();
 
+  /**
+   * Gets players.
+   *
+   * @return the players
+   */
   public List<Player> getPlayers() {
     return players;
   }
 
+  /**
+   * Sets players.
+   *
+   * @param players the players
+   */
   public void setPlayers(List<Player> players) {
     this.players = players;
   }
 
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
   public Long getId() {
     return id;
   }
 
+  /**
+   * Gets stacks.
+   *
+   * @return the stacks
+   */
   public List<Stack> getStacks() {
     return stacks;
   }
 
+  /**
+   * Sets stacks.
+   *
+   * @param stacks the stacks
+   */
   public void setStacks(List<Stack> stacks) {
     this.stacks = stacks;
   }
