ce31f4cc992a2eb9f7b16d87b01a4687b00986e6
dannyseymour <daniel@discordia.io>
2019-10-31 15:05:13 -0600

    added controller and put location methods in location entity

--- src/main/java/edu/cnm/deepdive/dominionservice/Methods.java
+++ src/main/java/edu/cnm/deepdive/dominionservice/Methods.java
@@ -98,34 +98,7 @@ public class Methods {
   }
 
   /** Location methods */
-  public Card getTopCard(Location fromWhere){
-    //gets the top card from a deck, discard, or stack. Will always return the same card for stacks, but
-    //decks and discards will be different
-    LocationType type = fromWhere.getLocationType();
-    if (fromWhere.hasCards) {
-      switch (type) {
-        case STACK:
-          break;
-        case DECK:
-          break;
-        case DISCARD:
-          break;
-        default:
-          //return invalid action error
-          break;
-      }
-    }
+ 
 
-  }
-
-  public boolean hasCards(Location fromWhere){
-    //this will get the length of the arraylist in a locations object (cards). Probably won't work
-    //until we put this method in the location class.
-    if(cards.size==0){
-      return false;
-    }else{
-      return true;
-    }
-
-  }
+  
 }
---
+++ src/main/java/edu/cnm/deepdive/dominionservice/controller/PlayerController.java
@@ -0,0 +1,5 @@
+package edu.cnm.deepdive.dominionservice.controller;
+
+public class PlayerController {
+
+}
